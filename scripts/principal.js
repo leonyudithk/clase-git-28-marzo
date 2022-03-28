ec951ba07181467240ba84976b46a7d30acb2ec4
isaac <isaac100081@hotmail.com>
2022-03-28 16:55:26 -0500

    Tarjetica

--- scripts/principal.js
+++ scripts/principal.js
@@ -1,2 +1,20 @@
+ 
 
-console.log('bienvenidos F-10')
+ document.addEventListener('DOMContentLoaded', ()=>{
+    let div = document.getElementById('f10')
+    div.innerHTML = `
+    <div class="card" style="width: 18rem;">
+  <img src="https://avatars.githubusercontent.com/u/98240755?s=400&u=b5dcb5e18612fb859484164b2b6f172f4667759d&v=4" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Isaac Gomez</h5>
+    </ul>
+    <ul class="card-text"><li>Tengo 20 años</li>
+    <li>Al comenzar ninguno, ahora pues no mucho</li>
+    <li>soy de Bogota</li>
+    <li>Me gusta mucho programar, la musica, etc.</li>
+
+    </ul>
+  </div>
+</div>
+    `
+ })
