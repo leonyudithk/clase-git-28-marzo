005184762bd48bdfcb747e55ec9e2c4edcdaa908
karvaroz <vargasriosk@gmail.com>
2022-03-28 16:58:26 -0500

    Subiendo mi script -K

--- scripts/karina.js
+++ scripts/karina.js
@@ -1,3 +1,6 @@
 const fullname = "Karina Vargas Rios";
 const age = 25;
-const experience = "si, en JavaScript y Python";"; 
+const experience = "si, en JavaScript y Python";
+const location = "Medellín, Colombia";
+const image =
+  "https://media-exp1.licdn.com/dms/image/C5603AQGOiSeAhCMmng/profile-displayphoto-shrink_200_200/0/1639787498943?e=1652918400&v=beta&t=4_P-tp59VsacDiYwSqzmOu2Yn49a4WGpN03KYdvIHp0";
