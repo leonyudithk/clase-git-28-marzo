27460d98d4aa80468ddb833582afc4cb55a29217
Bethsyf <bethsyfalcon@gmail.com>
2022-03-28 17:38:40 -0500

    presentacion personal

--- scripts/bethsyf.js
+++ scripts/bethsyf.js
@@ -1,1 +1,12 @@
-console.log('hola');
+document.addEventListener('DOMContentLoaded', ()=>{
+let cardBet = document.getElementById("f10");
+
+cardBet.innerHTML=`<div class="card" style="width: 18rem;">
+<img src="https://image.shutterstock.com/image-vector/businesswoman-technology-connectivity-concept-vector-260nw-165745979.jpg" class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">Bethsy Falcon</h5>
+  <p class="card-text">Tengo 39 años, he tenido un poco de contacto con la programacion con Python y Java, soy venezolana y actualmente resido en Medellin, me gusta mucho la tecnologia en especial lo relacionado a la web </p>
+  </div>
+</div>`
+
+})
