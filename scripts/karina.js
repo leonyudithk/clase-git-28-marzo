1e518d8d66bf38b8cfc7067caf1ddfe2f97ba63c
karvaroz <vargasriosk@gmail.com>
2022-03-28 17:45:38 -0500

    Subiendo mi script -K

--- scripts/karina.js
+++ scripts/karina.js
@@ -4,3 +4,18 @@ const experience = "si, en JavaScript y Python";
 const location = "Medellín, Colombia";
 const image =
   "https://media-exp1.licdn.com/dms/image/C5603AQGOiSeAhCMmng/profile-displayphoto-shrink_200_200/0/1639787498943?e=1652918400&v=beta&t=4_P-tp59VsacDiYwSqzmOu2Yn49a4WGpN03KYdvIHp0";
+
+const presentation = `
+    <div class="card" style="width: 18rem;">
+        <img src="${image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${fullname}</h5><br>
+            <h5 class="card-title">edad: ${age}</h5>
+            <h5 class="card-title">De: ${location}</h5>
+            <p class="card-text">${experience}</p>
+        </div>
+    </div>
+`;  
+const div10 = document.getElementById("10");
+div10.innerHTML(presentation)
+
