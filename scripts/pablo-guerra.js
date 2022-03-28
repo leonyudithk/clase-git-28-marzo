65716e02238ef9dfa095eb18a1f5ffe477272ee6
Pablo Guerra <pguerrabedoya@gmail.com>
2022-03-28 17:45:43 -0500

    carta pablo-guerra

---
+++ scripts/pablo-guerra.js
@@ -0,0 +1,15 @@
+// Capturando el contenedor f10 
+const f10 = document.getElementById("f10");
+
+f10.innerHTML = `<div class="card" style="width: 18rem;">
+                  <img src="https://scontent.feoh3-1.fna.fbcdn.net/v/t1.6435-9/178720143_4040169909383498_791390372834488449_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeFtkjGl0vcBII4rZw977DPNTyDR0x0vxIBPINHTHS_EgGubDh8_WXw9Ta7e6KC8B3Al7gEUx8pEoapL_q3_yluT&_nc_ohc=DNHJKuLrcHkAX8BCegx&_nc_ht=scontent.feoh3-1.fna&oh=00_AT_7ob_us9LufO6RQCoi3bKi122IgivZA5AuqdY11Fbrew&oe=62680AF7" alt="...">
+                  <div class="card-body">
+                    <h4 class="card-text">Nombre: Pablo Guerra</h4>
+                    <p class="card-text">Edad: 24 años</p>
+                    <p class="card-text">Lugar de residencia: Medellin</p>
+                    <p class="card-text">Con conocimientos basicos en javascript</p>
+                  </div>
+                </div>`
+
+
+
