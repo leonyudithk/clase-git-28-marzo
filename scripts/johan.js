6e98825af7abebe395939008bb9a199a335b3f87
Johan <johanp877@gmail.com>
2022-03-28 18:10:31 -0500

    adición de tarjeta desde JS

--- scripts/johan.js
+++ scripts/johan.js
@@ -1,1 +1,22 @@
 console.log('prueba')
+
+
+document.addEventListener('DOMContentLoaded', function() {
+let contenedor = document.getElementById('f10')
+
+
+
+contenedor.innerHTML += `
+<div  class="johan">
+<div class="card" style="width: 18rem;">
+   <img src="https://avatars.githubusercontent.com/u/85625991?v=4" class="card-img-top" alt="..."> 
+  <div class="card-body">
+    <h5 class="card-title">Johan Castañeda</h5>
+    <p class="card-text">Tengo 18 años y vivo en Bogotá, me apasiona la programación y el área de la tecnología en general, algunos de mis pasatiempos son los videojuegos y la música</p>
+    <a href="https://github.com/Johanp77" class="btn btn-primary">Go GitHub</a>
+  </div>
+</div>
+</div>
+`
+
+})
