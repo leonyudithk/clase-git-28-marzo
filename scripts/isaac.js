408f04ae9233dab218540d0abeeeee7029e69c8d
isaac <isaac100081@hotmail.com>
2022-03-28 17:02:42 -0500

    cambios F

--- scripts/isaac.js
+++ scripts/isaac.js
@@ -2,14 +2,17 @@
  document.addEventListener('DOMContentLoaded', ()=>{
     let div = document.getElementById('f10')
     div.innerHTML = `
-    <div class="card" style="width: 18rem;">
+    <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
   <img src="https://avatars.githubusercontent.com/u/98240755?s=400&u=b5dcb5e18612fb859484164b2b6f172f4667759d&v=4" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Isaac Gomez</h5>
     </ul>
     <ul class="card-text"><li>Tengo 20 años</li>
+    <h5>Conocimiento:</h3>
     <li>Al comenzar ninguno, ahora pues no mucho</li>
+    <h5>Ciudad:</h3>
     <li>soy de Bogota</li>
+    <h5>Gustos:</h3>
     <li>Me gusta mucho programar, la musica, etc.</li>
 
     </ul>
