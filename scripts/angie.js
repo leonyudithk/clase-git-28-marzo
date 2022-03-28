c003ec36a783fe1461b99fa2332fdf66560e22d8
Angie Gordillo <scarletrockbell@gmail.com>
2022-03-28 17:46:58 -0500

    card end

--- scripts/angie.js
+++ scripts/angie.js
@@ -1,5 +1,19 @@
 let presentacion = document.getElementById ("f10");
-let image = document.getElementById("img");
 
 
-image.src = 'img/me.jpeg';
+presentacion.innerHTML += `
+
+<div class="card text-white bg-primary mb-3" style="max-width: 18rem">
+<div class="card-header"><b>ANGIE LORENA GORDILLO ROA</b> </div>
+<div class="card-body">
+<img class="card-img" src="img/me.jpeg" alt="Card image">
+  <p class="card-text">Habia programado anteriormente en la parte de Backend,<br>
+  con PHP, con Framework como Laravel, y he manejado CMR tales como <br>
+  Wordpress y Umbraco, este ultimo manejado con C#</p>
+  <p> Me encuentro ubicada actualmente en Bogotá</p>
+  <img class="card-img" src="img/wordpress.jpg" alt="Card image">
+</div>
+</div>
+`
+
+
