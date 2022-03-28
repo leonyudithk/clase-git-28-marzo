d50cea71af57c94a65d4be467f653b8894773841
AlejandraRoc <alejandra_rocha@outlook.es>
2022-03-28 18:18:26 -0500

    Creacion tarjeta

--- scripts/Alejandra.js
+++ scripts/Alejandra.js
@@ -1,1 +1,10 @@
-console.log("hola")
+
+let f10 = document.getElementById('f10');
+let HTMLString = `
+<div class="card" style="width: 18rem;">
+  <img src="https://i.ytimg.com/vi/K0b8hFp6KI0/maxresdefault.jpg" class="card-img-top" alt="aleja">
+  <div class="card-body">
+    <p class="card-text">Mi nombre es Alejandra, tengo 24 años y no tengo experiencia en programación </p>
+  </div>
+</div>`;
+f10.innerHTML = HTMLString;
